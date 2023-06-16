c867fd7346a9f134c890ef3cb8d8ed98c7f2b1cc
MrUnwonted <arjungodzarjun@gmail.com>
2023-06-16 12:24:24 +0530

    SAQ

---
+++ SAQ/src/SAQ/Main.java
@@ -0,0 +1,7 @@
+package SAQ;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ SAQ/src/SAQ/Stack.java
@@ -0,0 +1,10 @@
+package SAQ;
+
+public class Stack {
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        
+    }
+}
