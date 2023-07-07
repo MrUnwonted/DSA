690535affde5c0d7dc2df937de9e0317df70d1ca
MrUnwonted <arjungodzarjun@gmail.com>
2023-07-07 14:50:16 +0530

    Search trie

--- NonLinear/src/Tree/Trie.java
+++ NonLinear/src/Tree/Trie.java
@@ -21,6 +21,7 @@ public class Trie {
     }
 
     public void insert(String word){
+        word = word.toLowerCase();
         TrieNode current = root;
         for (int i = 0; i <word.length() ; i++) {
             char ch = word.charAt(i);
@@ -33,6 +34,32 @@ public class Trie {
             current = node;
         }
         current.isendOfWord = true;
+    }
+
+    public boolean search(String word){
+       word = word.toLowerCase();
+        TrieNode current = root;
+        for (int i = 0; i <word.length() ; i++) {
+            char ch = word.charAt(i);
+            TrieNode node = current.children.get(ch);
+
+            if (node == null){
+                return false;
+            }
+            current = node;
+        }
+        return current.isendOfWord;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+//        String s = "Arjun";
+        trie.insert("Arjun");
+//        trie.search("Arjun");
+        if (!trie.search("ARJUN")){
+            System.out.println("No match");
+        } else System.out.println("Word matches");
+
     }
 
 }
